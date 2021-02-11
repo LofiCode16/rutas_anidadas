fd7ee58167d8c741314692f8ba9a46cdaf0d5834
Diego Perez <diegopr178@gmail.com>
2021-02-11 10:56:38 -0300

    add building views

---
+++ app/controllers/buildings_controller.rb
@@ -0,0 +1,27 @@
+class BuildingsController < ApplicationController
+
+    def index
+        if params[:q]
+            @buildings = Building.where('name LIKE ?', "%#{params[:q]}%")
+        else
+            @buildings = Building.all
+        end
+    end
+
+    def new
+        @building = Building.new
+    end
+
+    def create
+        @building = Building.new(building_params)
+
+        @building.save
+        redirect_to buildings_index_path
+    end
+
+    private
+    
+    def building_params
+        params.require(:building).permit(:name, :address)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'apartments/new', to: 'apartments#new'
-  post 'apartments', to: 'apartments#create'
+  # get 'buildings/index'
+  # get 'buildings/new', to: 'buildings#new'
+  # post 'buildings', to: 'buildings#create'
+  resources :buildings, only: [:index, :new, :create]
 
-  root 'apartments#index'
+  # get 'apartments/index'
+  # get 'apartments/new', to: 'apartments#new'
+  # post 'apartments', to: 'apartments#create'
+  resources :apartments, only: [:index, :new, :create]
+
+  root 'buildings#index'
 end
